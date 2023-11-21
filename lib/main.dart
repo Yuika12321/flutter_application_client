cc659fa249d446be70f82917ea121b2abca86317
aasdf <aasdfasfadfasd>
2023-11-21 18:35:44 +0900

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 import 'firebase_options.dart';
+import 'package:intl/intl.dart';
 
 var db = FirebaseFirestore.instance;
 String categoryCollectionName = 'cafe_category';
@@ -52,7 +53,7 @@ class _MainState extends State<Main> {
   PanelController panelController = PanelController();
 
   String toCurrency(int n) {
-    return '통화모습 숫자';
+    return NumberFormat.currency(locale: 'ko_KR', symbol: '₩').format(n);
   }
 
   // 카테고리 보기 기능
@@ -70,56 +71,21 @@ class _MainState extends State<Main> {
             return const Text('nothing');
           } else {
             return CustomRadioButton(
+              defaultSelected: 'toAll',
               elevation: 0,
               absoluteZeroSpacing: true,
               unSelectedColor: Theme.of(context).canvasColor,
-              buttonLables: [for (var data in datas) data['categoryName']],
-              buttonValues: [for (var data in datas) data.id],
+              buttonLables: [
+                '전체보기',
+                for (var data in datas) data['categoryName']
+              ],
+              buttonValues: ['toAll', for (var data in datas) data.id],
               buttonTextStyle: const ButtonTextStyle(
                   selectedColor: Colors.white,
                   unSelectedColor: Colors.black,
                   textStyle: TextStyle(fontSize: 16)),
               radioButtonValue: (value) {
-                itemList = FutureBuilder(
-                  future: db
-                      .collection(categoryCollectionName)
-                      .where('categoryId', isEqualTo: value)
-                      .get(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData == true) {
-                      var items = snapshot.data!.docs;
-                      if (items.isEmpty) {
-                        // 아이템이 없는 경우
-                        return const Center(
-                          child: Text('empty'),
-                        );
-                      } else {
-                        // 아이템이 있는 경우
-                        List<Widget> lt = [];
-                        for (var item in items) {
-                          lt.add(Container(
-                            child: Column(children: [
-                              Text(
-                                item['itemName'],
-                              ),
-                              Text(
-                                item['itemPrice'].toString(),
-                              ),
-                            ]),
-                          ));
-                        }
-                        return Wrap(
-                          children: lt,
-                        );
-                      }
-                    } else {
-                      // 아직 데이터 로드 중
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                  },
-                );
+                getItems(value);
               },
               selectedColor: Theme.of(context).colorScheme.secondary,
             );
@@ -132,6 +98,61 @@ class _MainState extends State<Main> {
   }
 
   // 아이템 보기 기능
+  Future<void> getItems(String value) async {
+    setState(() {
+      itemList = FutureBuilder(
+        future: value != 'toAll'
+            ? db
+                .collection(itemCollectionName)
+                .where('categoryId', isEqualTo: value)
+                .get()
+            : db.collection(itemCollectionName).get(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData == true) {
+            var items = snapshot.data!.docs;
+            if (items.isEmpty) {
+              // 아이템이 없는 경우
+              return const Center(
+                child: Text('empty'),
+              );
+            } else {
+              // 아이템이 있는 경우
+              List<Widget> lt = [];
+              for (var item in items) {
+                lt.add(Container(
+                  width: 200,
+                  height: 200,
+                  margin: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: Colors.green),
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          item['itemName'],
+                        ),
+                        Text(
+                          toCurrency(item['itemPrice'].to),
+                        ),
+                      ]),
+                ));
+              }
+              return Wrap(
+                children: lt,
+              );
+            }
+          } else {
+            // 아직 데이터 로드 중
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      );
+    });
+  }
 
   // 장바구니 보기 기능
 
@@ -140,6 +161,7 @@ class _MainState extends State<Main> {
     // TODO: implement initState
     super.initState();
     showCategoryList();
+    getItems('toAll');
   }
 
   @override
